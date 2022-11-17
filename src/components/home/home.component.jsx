b718e73a80bff53337e429a247a5803856b9af86
Djordje <djordjemilic33@gmail.com>
2022-11-17 20:53:17 +0100

    Added nav-bar and home components

---
+++ src/components/home/home.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './home.module.scss'
+import Navbar from '../nav-bar/nav-bar.component';
+
+function Home() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+
+export default Home;
---
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import styles from "./nav-bar.module.scss";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+function Navbar() {
+  // Mobile state
+  const [navOpen, setNavOpen] = useState(false);
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.logoContainer}>
+        <span className={styles.logo}>Food</span>
+      </div>
+
+      <div className={styles.mobileBars} onClick={() => setNavOpen(true)}>
+        <FaBars />
+      </div>
+
+      <ul
+        className={`${navOpen ? styles.menuMobileActive : undefined} ${
+          styles.menu
+        }`}
+      >
+        <li className={styles.mobileFaTimes} onClick={() => setNavOpen(false)}>
+          <FaTimes />
+        </li>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Reservation</li>
+        <li>News</li>
+        <li>More</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
